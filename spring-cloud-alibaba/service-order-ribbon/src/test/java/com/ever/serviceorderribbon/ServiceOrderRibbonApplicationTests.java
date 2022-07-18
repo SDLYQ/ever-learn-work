265d142e5ee845cccaa419c1faf605976556282d
hzt_xpr <1419901388@qq.com>
2022-07-18 16:44:35 +0800

    微服务：生产者（ribbon） 调用消费者

---
+++ spring-cloud-alibaba/service-order-ribbon/src/test/java/com/ever/serviceorderribbon/ServiceOrderRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ever.serviceorderribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOrderRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
