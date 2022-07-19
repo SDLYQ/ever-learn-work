5daa248b5f0c9b08dbf06712cff9fbfcbbaf24c0
hzt_xpr <1419901388@qq.com>
2022-07-19 10:40:47 +0800

    微服务：sentinel核心库流量控制

---
+++ spring-cloud-alibaba/service-sentinel-core/src/main/java/com/ever/servicesentinelcore/ServiceSentinelCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ever.servicesentinelcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSentinelCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSentinelCoreApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-alibaba/service-sentinel-core/src/test/java/com/ever/servicesentinelcore/ServiceSentinelCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ever.servicesentinelcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSentinelCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
